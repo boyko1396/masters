27366821ad0490ae0da82b52081d75d0a53952a5
Vasya <boyko1396@gmail.com>
2022-10-15 16:31:29 +0300

    with adaptive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
-  // header dropdown toggle
-  $('.js-work-road-moore').click(function(){
+  // tariffs toggle
+  $('.js-a-tariffs-table-toggle').click(function(){
+    $(this).toggleClass('is-active');
+    $(this).parents('table').find('tr.is-hidden').toggleClass('is-active');
+  });
+
+  // work road toggle
+  $('.js-a-work-road-moore').click(function(){
     $(this).toggleClass('is-active');
     $('.a-work-road__list').toggleClass('is-active');
     $('.a-work-road__list-item.is-hidden').toggleClass('is-active');
   });
-  // header dropdown toggle
-  $('.js-work-road-moore-inline').click(function(){
-    $(this).toggleClass('is-active');
-    $('.a-work-r').toggleClass('is-active');
-  });
-  // header dropdown toggle
-  $('.js-tariffs-table-toggle').click(function(){
-    $(this).toggleClass('is-active');
-    $(this).parents('table').find('tr.is-hidden').toggleClass('is-active');
-  });
+
+  // slick slider init
+  if ($('.js-a-tariffs-slider-init')[0]){
+    $('.js-a-tariffs-slider-init').slick({
+      arrows: false,
+      adaptiveHeight: true,
+      centerMode: true,
+      dots: true,
+      variableWidth: true
+    });
+  }
 });
